a62f09c8d88c04e56c2fa88c8a3a2c11933efe3d
unknown <yacineboudebbouz7@gmail.com>
2023-07-01 12:20:00 -0700

    database provider

---
+++ lib/models/database_provider.dart
@@ -0,0 +1,62 @@
+import 'package:expenseapp/constants/icons.dart';
+import 'package:expenseapp/models/ex_category.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseProvider {
+  List<ExpenseCategory> _categories = [];
+  List<ExpenseCategory> get categories => _categories;
+  Database? _database;
+  Future<Database> get database async {
+    // databse directory
+    final dbDirectory = await getDatabasesPath();
+    // database name
+    const dbName = 'expense.db';
+    // the full path
+    final path = join(dbDirectory, dbName);
+
+    _database = await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDb,
+    );
+    return _database!;
+  }
+
+  static const cTable = 'categoryTable';
+  static const eTable = 'expenseTable';
+  Future<void> _createDb(Database db, int version) async {
+    await db.transaction((txn) async {
+      await txn.execute(''' CREATE TABLE $cTable(
+      title TEXT,
+      entries INTEGER
+      totalAmount TEXT
+    )''');
+      await txn.execute('''CREATE TABLE $eTable(
+      id INTEGER,
+      title TEXT,
+      anmount TEXT,
+      date TEXT,
+      category TEXT
+    )''');
+      for (int i = 0; i < icons.length; i++) {
+        await txn.insert(cTable, {
+          'title': icons.keys.toList(),
+          'entries': 0,
+          'totalAmount': (0.0).toString(),
+        });
+      }
+    });
+  }
+
+  Future<List<ExpenseCategory>> fetchCategories() async {
+    final db = await database;
+    return await db.transaction((txn) async {
+      return await txn.query(cTable).then((data) {
+        final converted = List<Map<String, dynamic>>.from(data);
+        List<ExpenseCategory> nList = List.generate(converted.length,
+            (index) => ExpenseCategory.fromString(converted[index]));
+      });
+    });
+  }
+}
