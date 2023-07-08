28e04c5d05b5fa629310a76191e8d65fedc8be26
unknown <yacineboudebbouz7@gmail.com>
2023-07-08 21:44:07 +0200

    total charts

--- lib/widgets/expense_screen/expense_chart.dart
+++ lib/widgets/expense_screen/expense_chart.dart
@@ -1,6 +1,7 @@
 import 'package:expenseapp/models/database_provider.dart';
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 
 class ExpenseChart extends StatefulWidget {
@@ -19,16 +20,37 @@ class _ExpenseChartState extends State<ExpenseChart> {
         var maxY = db.calculateEntriesAndAmount(widget.category)['totalAmount'];
         var list = db.calculateWeekExpenses().reversed.toList();
         return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
-          child: BarChart(BarChartData(minY: 0, maxY: maxY, barGroups: [
-            ...list.map((e) => BarChartGroupData(x: list.indexOf(e), barRods: [
-                  BarChartRodData(
-                      toY: e['amount'],
-                      width: 20.0,
-                      borderRadius: BorderRadius.zero,
-                      color: Colors.primaries[list.indexOf(e)])
-                ]))
-          ])),
+          padding: const EdgeInsets.all(25),
+          child: BarChart(
+            BarChartData(
+              minY: 0,
+              maxY: maxY,
+              barGroups: [
+                ...list.map(
+                  (e) => BarChartGroupData(
+                    x: list.indexOf(e),
+                    barRods: [
+                      BarChartRodData(
+                        toY: e['amount'],
+                        width: 20.0,
+                        borderRadius: BorderRadius.zero,
+                      )
+                    ],
+                  ),
+                ),
+              ],
+              titlesData: FlTitlesData(
+                  topTitles: const AxisTitles(drawBelowEverything: true),
+                  // leftTitles: const AxisTitles(drawBelowEverything: true),
+                  rightTitles: const AxisTitles(drawBelowEverything: true),
+                  bottomTitles: AxisTitles(
+                      sideTitles: SideTitles(
+                    showTitles: true,
+                    getTitlesWidget: (value, meta) =>
+                        Text(DateFormat.E().format(list[value.toInt()]['day'])),
+                  ))),
+            ),
+          ),
         );
       },
     );
